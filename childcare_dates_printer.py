ebd570adb5cef8ee1add6462765eb39848b74685
Jangboo Lee <github@jangboolee.com>
2024-02-07 05:37:07 +0900

    Change program to read from CSV

--- childcare_dates_printer.py
+++ childcare_dates_printer.py
@@ -1,40 +1,19 @@
-import re
-from datetime import datetime
+from csv import DictReader
 from tabulate import tabulate
 
 
-def get_dates() -> tuple:
+def read_assignment_file(file_name: str = "childcare_assignment.csv") -> tuple:
 
-    date_pattern = r"202[4-9]-[01]\d-[0-3]\d"
+    assignments = []
+    with open(file_name, "r") as f:
+        reader = DictReader(f)
+        for row in reader:
+            assignments.append([row["date"], row["AM_PM"], row["helper"]])
 
-    while True:
-        start_date_str = input("Enter start date (YYYY-MM-DD): ")
-        if re.match(date_pattern, start_date_str):
-            while True:
-                end_date_str = input("Enter end date (YYYY-MM-DD): ")
-                if re.match(date_pattern, end_date_str):
-                    print(f"Start: {start_date_str} | End: {end_date_str}")
-                    break
-                else:
-                    print("Enter in valid ISO format (YYYY-MM-DD)")
-            break
-        else:
-            print("Enter in valid ISO format (YYYY-MM-DD)")
-
-    dates = (
-        datetime.strptime(start_date_str, "%Y-%m-%d"),
-        datetime.strptime(end_date_str, "%Y-%m-%d"),
-    )
-
-    return dates
-
-
-def get_caretakers(dates: tuple) -> list:
-    duration = dates[1] - dates[0]
-    print(duration.days)
+    return assignments
 
 
 if __name__ == "__main__":
 
-    dates = get_dates()
-    get_caretakers(dates)
+    assignments = read_assignment_file()
+    print(tabulate(assignments, headers=["date", "AM/PM", "helper"]))
